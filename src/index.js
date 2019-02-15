b565158aef9adb1416283c7083e8da26dc8ce0f3
Mayuri Rathod <mayuri.gr@gmail.com>
2019-02-15 10:39:07 +0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { CommentDetail } from "./CommentDetail";
 
-const App=()=>{
-    return(
-        <div>Hii </div>
-    )
+export const App = () => {
+  return (
+    <div className="ui container comments">
+      <CommentDetail name={"sam"} time={"7.00 pmToday"} />
+    </div>
+  );
 };
-ReactDOM.render(<App/>,document.querySelector("#root"));
-
+ReactDOM.render(<App />, document.querySelector("#root"));
