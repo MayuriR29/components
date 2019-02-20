094311ecfd8e51b38ce3d36d9655b1faad7ee6d2
Mayuri Rathod <mayuri.gr@gmail.com>
2019-02-20 11:33:25 +0800

    Added Approvalm card

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { CommentDetail } from "./CommentDetail";
+import { ApprovalCard } from "./ApprovalCard";
+
+import faker from "faker";
 
 export const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="sam" time="2 p.m Monday" />
-      <CommentDetail author="sam" time="2 p.m Monday" />
-
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="2 p.m Monday"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Tom"
+          timeAgo="2 p.m Monday"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jerry"
+          timeAgo="2 p.m Monday"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
